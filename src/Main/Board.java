164291e4fb33bc0643fb599dbe4eef5900cba9d2
Maha <sm2mahaqa@gmail.com>
2023-03-01 12:33:33 -0500

    tic-tac-toe version 1

--- src/Main/Board.java
+++ src/Main/Board.java
@@ -1,21 +1,57 @@
 package Main;
 
 public class Board {
-    // a 2D array to represent the pieces on the board:
-    char [][] board;
+    public String[][] board;
 
-    // should instantiate the board to be a 3x3 array:
     public Board() {
-
+        board = new String[3][3];
+        for (int row = 0; row < 3; row++) {
+            for (int col = 0; col < 3; col++) {
+                board[row][col] = " ";
+            }
+        }
     }
 
-    // this method should take in a row, column, and a newChar and update the board accordingly:
-    public void updateBoard(int row, int column, char newChar) {
-
+    public void printBoard() {
+        for (int row = 0; row < 3; row++) {
+            System.out.println(board[row][0] + "|" + board[row][1] + "|" + board[row][2]);
+            if (row < 2) {
+                System.out.println("-+-+-");
+            }
+        }
     }
 
-    // this method should return a string representation of the board:
-    public String toString() {
-        return "";
+    public boolean checkWin() {
+        // Check rows
+        for (int row = 0; row < 3; row++) {
+            if (board[row][0].equals(board[row][1]) && board[row][1].equals(board[row][2]) && !board[row][0].equals(" ")) {
+                return true;
+            }
+        }
+        // Check columns
+        for (int col = 0; col < 3; col++) {
+            if (board[0][col].equals(board[1][col]) && board[1][col].equals(board[2][col]) && !board[0][col].equals(" ")) {
+                return true;
+            }
+        }
+        // Check diagonals
+        if (board[0][0].equals(board[1][1]) && board[1][1].equals(board[2][2]) && !board[0][0].equals(" ")) {
+            return true;
+        }
+        if (board[0][2].equals(board[1][1]) && board[1][1].equals(board[2][0]) && !board[0][2].equals(" ")) {
+            return true;
+        }
+        return false;
+    }
+
+    public boolean checkDraw() {
+        for (int row = 0; row < 3; row++) {
+            for (int col = 0; col < 3; col++) {
+                if (board[row][col].equals(" ")) {
+                    return false;
+                }
+            }
+        }
+        return true;
     }
 }
--- src/Main/Player.java
+++ src/Main/Player.java
@@ -1,23 +1,41 @@
 package Main;
 
+import java.util.Scanner;
+
 public class Player {
     private String name;
-    // gamePiece refers to which character will be placed on the board to indicate
-    // that the player claims that spot:
-    private char gamePiece;
+    private String symbol;
 
-    // this constructor should take in a name and a game piece and assign the values to the fields:
-    public Player(String name, char gamePiece) {
-
+    public Player(String name, String symbol) {
+        this.name = name;
+        this.symbol = symbol;
     }
 
-    // should return the player's name:
     public String getName() {
-        return "";
+        return name;
     }
 
-    // should return the game piece of the player:
-    public char getGamePiece() {
-        return 0;
+    public String getSymbol() {
+        return symbol;
     }
+
+    public void play(Board board) {
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            System.out.print(name + ", enter row (1-3): ");
+            int row = scanner.nextInt() - 1;
+            System.out.print(name + ", enter column (1-3): ");
+            int col = scanner.nextInt() - 1;
+            if (row < 0 || row > 2 || col < 0 || col > 2) {
+                System.out.println("Invalid input, please try again.");
+            } else if (!board.board[row][col].equals(" ")) {
+                System.out.println("That spot is already taken, please try again.");
+            } else {
+                board.board[row][col] = symbol;
+                board.printBoard();
+                break;
+            }
+        }
+    }
+
 }
